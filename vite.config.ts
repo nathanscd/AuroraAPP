84db2faf7b8bca96d264c1ab1c4030fd666b10ba
nathanscd <nathansec26@gmail.com>
2026-06-15 14:31:37 -0300

    other test

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { jsxLocPlugin } from "@builder.io/vite-plugin-jsx-loc";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
@@ -30,7 +29,6 @@ export default defineConfig(({ command }) => ({
       jsxRuntime: 'automatic',
     }),
     tailwindcss(),
-    command === "serve" ? jsxLocPlugin() : null,
     googleDocsProxyPlugin()
   ].filter(Boolean),
   resolve: {
